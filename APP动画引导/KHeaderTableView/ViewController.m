d4872ce7f8d9b494e19451527ab47ff56e8bf5c9
凡 <wangf@morningtec.cn>
2017-12-06 10:43:03 +0800

    Fix

--- APP动画引导/KHeaderTableView/ViewController.m
+++ APP动画引导/KHeaderTableView/ViewController.m
@@ -18,7 +18,6 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-[UIView animateWithDuration:<#(NSTimeInterval)#> animations:<#^(void)animations#>]
     
     
 }
